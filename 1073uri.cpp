575f03b5782e5ca6b632d4126a2a8b2e7dd15472
Bishakha Biswas <tandra.bu@gmail.com>
2021-07-08 23:49:24 +0600

    commit

---
+++ 1073uri.cpp
@@ -0,0 +1,16 @@
+#include<iostream>>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    for(int i=2;i<=a;i+=2)
+    {
+
+
+            cout<<i<<"^"<<"2"<<" "<<"="<<" "<<i*i<<endl;
+
+    }
+
+}
+
