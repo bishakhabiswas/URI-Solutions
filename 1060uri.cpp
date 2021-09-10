011ae3fff50607dc44bf1e40ca5bca99ae1303a6
Bishakha Biswas <tandra.bu@gmail.com>
2021-09-11 00:01:29 +0600

    commit

---
+++ 1060uri.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int count=0;
+    float p;
+
+    for(int i=0;i<6;i++)
+    {   cin>>p;
+      if(p>0) count++;
+
+    }
+    cout<<count<<" "<<"valores positivos"<<endl;
+}
