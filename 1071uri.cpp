b0289c8028a7c16d35ef46ead9c13701346d6e8c
Bishakha Biswas <tandra.bu@gmail.com>
2021-09-14 23:51:18 +0600

    commit

---
+++ 1071uri.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,sum=0,i;
+    cin>>a>>b;
+    if(b<a)
+    {
+        swap(a,b);
+
+    }
+
+    for( i=a+1;i<b;i++)
+    {
+        if(i%2!=0)
+        {
+        sum += i;
+        }
+    }
+    cout<<sum;
+}
+
+
---
+++ 1072uri.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a;
+    int count1=0,count2=0;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>a;
+        if(a>=10 && a<=20)
+        {
+            count1++;
+        }
+   else
+        {
+            count2++;
+        }
+    }
+    printf("%d in",count1);
+    printf("%d out",count2);
+}
