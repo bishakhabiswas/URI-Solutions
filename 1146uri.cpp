c1a7540b715a0efd6be61d8b938577db2e0d9216
Bishakha Biswas <tandra.bu@gmail.com>
2021-09-25 00:37:48 +0600

    commit

---
+++ 1146uri.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+
+    while(1)
+    {
+        cin>>x;
+       if(x==0) break;
+       cout<<1;
+        for(int i=2;i<=x;i++)
+        {
+        cout<<" "<<i;
+
+
+        }
+          cout<<endl;
+
+
+    }
+
+
+}
